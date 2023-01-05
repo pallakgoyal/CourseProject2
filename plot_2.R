facefd844907c4a605b4ab51c367b6f3ec2e82ed
Pallak Goyal <107195476+pallakgoyal@users.noreply.github.com>
2023-01-05 11:38:19 +0530

    Add files via upload

--- plot_2.R
+++ plot_2.R
@@ -2,13 +2,9 @@
 NEI <- readRDS("./summarySCC_PM25.rds")
 SCC <- readRDS("./Source_Classification_Code.rds")
 ##create data frame with sum of pollutants for Baltimore city
-bt1999 <- sum(NEI[NEI$year == 1999 & NEI$fips == "24510", ]$Emissions)
-bt2002 <- sum(NEI[NEI$year == 2002 & NEI$fips == "24510", ]$Emissions)
-bt2005 <- sum(NEI[NEI$year == 2005 & NEI$fips == "24510", ]$Emissions)
-bt2008 <- sum(NEI[NEI$year == 2008 & NEI$fips == "24510", ]$Emissions)
-years <- c("1999","2002","2005","2008")
-btpm25 <- c(bt1999, bt2002, bt2005, bt2008)
-df2 <- data.frame(years, btpm25)
+df2 <- aggregate(NEI[NEI$fips == "24510", ]$Emissions, 
+                 by = list(Category = NEI[NEI$fips == "24510", ]$year), 
+                 FUN = sum)
 names(df2) = c("Years", "TotalPM25")
 ##create barplot
 png("./plot_2.png")
