dc0ddc56d868b097d3c05c08bbd6bed50d2e7617
Pallak Goyal <107195476+pallakgoyal@users.noreply.github.com>
2023-01-05 12:22:26 +0530

    Add files via upload

--- plot_3.R
+++ plot_3.R
@@ -1,3 +1,15 @@
 ##read data into R
 NEI <- readRDS("./summarySCC_PM25.rds")
 SCC <- readRDS("./Source_Classification_Code.rds")
+##create data frame
+df3 <- aggregate(NEI[NEI$fips == "24510", ]$Emissions, 
+                 by = list(Category = NEI[NEI$fips == "24510", ]$year, 
+                           Category = NEI[NEI$fips == "24510", ]$type), 
+                 FUN = sum)
+names(df3) <- c("Year", "Source", "Emissions")
+##create plot
+library(ggplot2)
+png("./plot_3.png")
+qplot(x=Year,y=Emissions, data = df3, facets = .~ Source, 
+      geom = c("point", "line"))  + theme_bw()
+dev.off()
